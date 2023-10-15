ecbc7432bb5ad5744f5e6b7565328a011f51ce57
AXEL LARESE <axelarese@gmail.com>
2023-10-15 10:55:11 +0200

    Exercice création d'une carte d'un film avec react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import FilmCard from './components/filmCard.jsx'
+import './components/filmCard.scss'
+
+function App() { 
+
+
+  return (
+    <>
+      <div>
+        <FilmCard />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/filmCard.jsx
@@ -0,0 +1,29 @@
+function FilmCard () {
+
+return (
+
+    <div>
+        <div className="global">
+            <img src="https://fr.web.img4.acsta.net/pictures/22/11/02/14/49/4565071.jpg" alt="Avatar Image" />
+                <div className="informations">
+                    <h1 className="avatarTitle">Avatar 2 - La voie de l&apos;eau</h1>
+                     <p className="decription">Se déroulant plus d&apos;une décennie après les événements relatés dans le premier film, AVATAR : LA VOIE DE L’EAU raconte l&apos;histoire des membres de la famille Sully (Jake, Neytiri et leurs enfants), les épreuves auxquelles ils sont confrontés, les chemins qu’ils doivent emprunter pour se protéger les uns les autres, les batailles qu’ils doivent mener pour rester en vie et les tragédies qu&apos;ils endurent. Jake Sully et Ney&apos;tiri ont formé une famille et font tout pour rester aussi soudés que possible. Ils sont cependant contraints de quitter leur foyer et d&apos;explorer les différentes régions encore mystérieuses de Pandora. Lorsqu&apos;une ancienne menace refait surface, Jake va devoir mener une guerre difficile contre les humains.
+
+</p>
+                    <div className="rate">
+                        <p>Note de la rédaction :</p>
+                        <img src="https://www.mavalise-vr.com/wp-content/uploads/2020/03/note-de-4-768x181.png.webp" alt="" />      
+                    </div>
+                </div>
+        </div>      
+    </div>
+
+
+
+)
+
+
+}
+
+
+export default FilmCard
